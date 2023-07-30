9d4a21cbafbed160b239d24e82d827bf5a5680ec
romain <r.neveu14@gmail.com>
2023-07-30 13:20:40 +0200

    use hash instead of url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Book from './player/Book';
 import BookEditor from './editor/BookEditor';
 import story from './books/ronin_malgre_vous.json';
